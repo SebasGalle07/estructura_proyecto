19b83c9067279421bb87cc7880a45f101405940e
lisamaya1 <lisamaya0709@gmail.com>
2025-05-14 10:47:11 -0500

    octavo Lisa

--- src/controller/AyudaController.java
+++ src/controller/AyudaController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import model.ColaPrioridad;
+import model.SolicitudAyuda;
+import model.Usuario;
+
 public class AyudaController {
+    private ColaPrioridad cola;
+
+    public AyudaController() {
+        this.cola = new ColaPrioridad();
+    }
 }
