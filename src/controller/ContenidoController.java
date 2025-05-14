d0f33d69979dd8d5b79a8550ea3f361b3f4a84a7
lisamaya1 <lisamaya0709@gmail.com>
2025-05-14 10:48:47 -0500

    noveno Lisa

--- src/controller/ContenidoController.java
+++ src/controller/ContenidoController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import model.ArbolBinarioBusqueda;
+import model.Contenido;
+import model.Usuario;
+import model.ListaEnlazada; // ✅ Import necesario para usar tu lista propia
+
 public class ContenidoController {
+    private ArbolBinarioBusqueda arbol;
+    private int idAuto = 1;
+
+    public ContenidoController() {
+        this.arbol = new ArbolBinarioBusqueda();
+    }
+
+    public Contenido publicarContenido(String titulo, String autor, String tema, String tipo, String enlace, Usuario usuario) {
+        Contenido nuevo = new Contenido(idAuto++, titulo, autor, tema, tipo, enlace);
+        arbol.insertar(nuevo);
+        usuario.agregarContenido(nuevo);
+        return nuevo;
+    }
 }
+
